07dbd236c75b970955aa2a6dc0c62a841966aa17
MOGWAmogwa <79387913+MOGWAmogwa@users.noreply.github.com>
2022-06-03 11:49:57 +0900

    JS tweak (clear code)

--- main.js
+++ main.js
@@ -1,43 +1,30 @@
-
-const number = document.querySelector('.rating');
-const submitBtn = document.querySelector('.submit');
-const request = document.querySelector('#request');
+const submit = document.querySelector('button');
 const thanks = document.querySelector('#thanks');
+const numbers = document.querySelectorAll('.number');
 const result = document.querySelector('.result');
 
 
-function colorChange(value){
-    value.classList.add('colorChange')
-}
 
-
-let num = [];
-let storage = [];
-number.addEventListener('click', (event)=>{
-
-    const target = event.target;
-    const ratingNum = target.dataset.id;
-    num.push(ratingNum);
-    storage.push(target)
-    colorChange(target);
-    if (num.length>1 && ratingNum!=num[num.length-2] ){
-        storage[storage.length-2].classList.remove('colorChange')
-    }
-})
-
-submitBtn.addEventListener('click', (event)=>{
-    if(num.length===0){
-        return;
-    }
-    thanks.classList.add('open')
+submit.addEventListener('click', (event)=>{
+    thanks.classList.add('open');
     result.innerHTML = `
-        <div class="result">
-            You selected ${num[num.length-1]} out of 5
-        </div>
+        You selected ${ratingResultList[ratingResultList.length-1]} out of 5
     `
 })
 
+let ratingResultList = [];
+numbers.forEach((num)=>{
+    num.addEventListener('click', (event)=>{
+        if(num.classList.contains('active')){
+            num.classList.toggle('active');
+        } else {
+            numbers.forEach((num)=>{
+                num.classList.remove('active');
+            })
+            num.classList.toggle('active')
+        }
 
-
-
+        ratingResultList.push(event.target.dataset.id)
+    })
+})
 
